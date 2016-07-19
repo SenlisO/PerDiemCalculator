fa92c8f00e99b3003f28118b118c73b37aa4c8a7
Richard Romick <richard@DebianVirtualbox>
2016-07-18 22:19:33 -0500

    more stuff for main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class Bank:
         self.end_date = date(1950, 1, 1)  # end date of travel, new years 2000 indicates no data
         self.travel_per_diem = 0  # per diem for travel days
         self.daily_per_diem = 0  # per diem for normal days
+        self.transactions = []
 
     def set_per_diem_data(self, begin_date, end_date, travel_per_diem, daily_per_diem):
         # sanitize date values
@@ -57,18 +58,39 @@ class Bank:
         total += (duration.days - 2) * self.daily_per_diem
         return total
 
+    def add_transaction(self, name, transaction_date, amount, remarks):
+        try:
+            self.transactions.append(Transaction(name, transaction_date, amount, remarks))
+        except InvalidOperationError as e:
+            print(e)
+
+        self.transactions = sorted(self.transactions, key=lambda Transaction: Transaction.transaction_date)
+
 
 class Transaction:
     # Class contains data for individual transactions
     # Data: date, name, amount, remarks
-    def __init__(self):
-        print("Placeholder")
-        # todo: create initialization function
+    def __init__(self, name, transaction_date, amount, remarks):
+        # first step: sanitize input
+        if not isinstance(name, str) or not isinstance(amount, float) or not isinstance(remarks, str):
+            raise InvalidOperationError("Debug error: Transaction init; values passed not valid type")
+        if not isinstance(transaction_date, date):
+            raise InvalidOperationError("Debug error: Transaction init; values passed not valid type")
+
+        if not amount > 0:
+            raise InvalidOperationError("Debug error: Transaction init; amount not greater than 0")
+
+        # set object values
+        self.name = name
+        self.transaction_date = transaction_date
+        self.amount = amount
+        self.remarks = remarks
 
 
 class GUI:
     def __init__(self):
         self.bank = Bank()  # variable is used for all interaction with Bank class
+        self.display_index = 0
 
     def create_test_data(self):
         # usage: create test data for debugging
@@ -80,8 +102,18 @@ class GUI:
         # set bank objects financial data
         try:
             self.bank.set_per_diem_data(begin_date, end_date, travel_per_diem, daily_per_diem)
+            self.bank.add_transaction("7-11", date(2016, 8, 15), 3.23, "coffee and donuts")
+            self.bank.add_transaction("Marriott Sunshine", date(2016, 8, 15), 98.0, "Hotel stay")
+            self.bank.add_transaction("Burgers and more", date(2016, 8, 15), 5.5, "Dinner")
+            self.bank.add_transaction("Some more burgarz", date(2016, 8, 16), 11.5, "Lunch")
+            self.bank.add_transaction("Forgot these burgerz", date(2016, 8, 15), 6.75, "More Lunches")
+            self.bank.add_transaction("More stuff to eat", date(2016, 8, 16), 10.75, "Lunchenator")
+            self.bank.add_transaction("Marriott Sunset", date(2016, 8, 16), 112.68, "Hotel stay")
+            self.bank.add_transaction("Marriott little dipper", date(2016, 8, 17), 95.12, "Hotel super")
+            self.bank.add_transaction("Marriott bigger lipper", date(2016, 8, 18), 97.00, "Hotel duper")
+
         except InvalidOperationError as e:
-            print (e.message)
+            print(e.message)
 
         # todo: create full test data
 
@@ -98,6 +130,7 @@ class GUI:
         self.clear_screen()
 
         print("Main Menu")
+        print("------------------------")
         print("Today's date: %s" % date.today())
         print("TDY start date: %s" % self.bank.begin_date)
         print("TDY end date: %s" % self.bank.end_date)
@@ -107,11 +140,23 @@ class GUI:
         except InvalidOperationError:
             print("")
 
+        print ("-------------------------------")
         print("Travel Per Diem amount: %s" % '${:,.2f}'.format(self.bank.travel_per_diem))
         print("Daily Per Diem amount: %s" % '${:,.2f}'.format(self.bank.daily_per_diem))
         print("Total Per Diem amount: %s" % '${:,.2f}'.format(self.bank.calculate_per_diem_total()))
 
-        # todo: display all transactions
+        # display transactions
+        print("-----------------------------------------------------------------")
+        print("# Date,        Name,                             Amount,    Remarks")
+
+        # todo: function to check display_index is valid
+        # last constant in for loop controls how many records are displayed at once
+        for i in range(self.display_index, self.display_index+5):
+            print("%s:%s   %s    %s     %s" % (i, self.bank.transactions[i].transaction_date,
+                                             '{:<30}'.format(self.bank.transactions[i].name),
+                                             '${:6,.2f}'.format(self.bank.transactions[i].amount),
+                                             self.bank.transactions[i].remarks))
+
         # todo: display total spent
         # todo: display total remaining
         # todo: display total spent vs earned for current date
@@ -196,7 +241,9 @@ class GUI:
                 bad_data = True
                 continue
 
+    def main_menu_menu(self):
+        choice = input("Enter [c]ange dates & Per Diem, [n]ew transaction, [#] modify transaction, [s]ave, [q]uit:")
 
 ui = GUI()
-# ui.create_test_data()
+ui.create_test_data()
 ui.start_ui()
