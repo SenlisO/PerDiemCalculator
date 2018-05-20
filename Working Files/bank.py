af4bbe6ca1290289328a69ce0602be9e0aaf6a75
Senlis <gizmo.romick@gmail.com>
2018-05-19 20:29:49 -0800

    Remove multiple trip logic from applicaion

--- Working Files/bank.py
+++ Working Files/bank.py
@@ -13,19 +13,16 @@ class Accountant:
     # Class contains transaction and perdiem data and performs operations on them
 
     def __init__(self):
-        self._tdy = []  # class Accountant built to contain multiple trips, if necessary
-        self._tdy.append(Trip())  # this will be the default trip the object will refer to if no trip is specified
-        
         self._transactions = []
 
-    
+
     def get_transaction_value(self, variable_name, transaction_index):
         """
         This function is a one stop shop for necessary variable data
         Input
             variable_name - name of the variable caller wants a value for
-            possibilities include: "begin date", "end date" "daily per diem" "travel per diem"
-            transaction_index = 0 - which trip the caller wants values for
+                possibilities include: "begin date", "end date" "daily per diem" "travel per diem"
+            transaction_index = 0 - which transaction the caller wants values for
         Return
             value of variable requested
         Throws
@@ -50,37 +47,6 @@ class Accountant:
         else: # step 4: if none of the previous checks are correct, the caller isn't asking for a valid variable
             raise ValueError("Debug error: Accountant.get_transaction_value variable does not exist")
 
-
-    def get_trip_value(self, variable_name, trip_index=0):
-        """
-        This function is a one stop shop for necessary variable data
-        Input
-            variable_name - name of the variable caller wants a value for
-            possibilities include: "begin date", "end date" "daily per diem" "travel per diem"
-            trip_index = 0 - which trip the caller wants values for
-        Return
-            value of variable requested
-        Throws
-            value error if name is incorrect
-        """
-
-        # step 1: pull dictionary variable containing trip's values from Trip method
-        pulled_values = self._tdy[trip_index].retrieve_values()
-
-        # step 2: determine what caller is asking for and return that value
-        try:
-            if variable_name == "begin date":
-                return pulled_values['begin_date']
-            elif variable_name == "end date":
-                return pulled_values['end_date']
-            elif variable_name == "daily per diem":
-                return pulled_values['daily_per_diem']
-            elif variable_name == "travel per diem":
-                return pulled_values['travel_per_diem']
-            else: # step 3: handle error scenarios
-                raise InvalidOperationError("Debug error: Accountant.get_trip_values: trip variable does not exist")
-        except InvalidOperationError as e:
-            raise e
 
     @staticmethod  # This means we can call the method without initializing an object
     def convert_to_date(d):
@@ -115,7 +81,7 @@ class Accountant:
 
     def load_data(self):
         """
-        This function loads data from the file ledger.txt 
+        This function loads data from the file ledger.txt
         Input
             none
         Returns
@@ -141,7 +107,6 @@ class Accountant:
 
         # todo: check file inputs for bad data and raise ValueError exception if necessary
         # todo: parse all file data before loading any of it into accountant object
-        # step 4: pull Trip object info from file_data array
         try:
             temp = file_data[0]
             begin_date = date(int(temp[0:4]), int(temp[4:6]), int(temp[6:]))
@@ -155,8 +120,8 @@ class Accountant:
 
             # step 5: set per diem and dates values in default TDY
             # todo: multiple TDY loading
-            self._tdy[0].set_per_diem(daily_per_diem, travel_per_diem)
-            self._tdy[0].set_dates(begin_date, end_date)
+            self._tdy.set_per_diem(daily_per_diem, travel_per_diem)
+            self._tdy.set_dates(begin_date, end_date)
 
             # step 6: iterate through each transaction pulled from the file
             i = 4  # index number,starts after previous reads for trip data is done
@@ -200,9 +165,8 @@ class Accountant:
         file = open("ledger.txt", "w")
 
         # step 2: write beginning and end date along with per diem values
-        # todo: support for multiple trips
         try:
-            pulled_values = self._tdy[0].retrieve_values()
+            pulled_values = self._tdy.retrieve_values()
             file.write(pulled_values['begin_date'].strftime("%Y%m%d") + "\n")
             file.write(pulled_values['end_date'].strftime("%Y%m%d") + "\n")
             file.write(str(pulled_values['travel_per_diem']) + "\n")
@@ -226,7 +190,7 @@ class Accountant:
         # step 4: close the file
         file.close()
 
-    def set_per_diem_data(self, begin_date, end_date, travel_per_diem, daily_per_diem, trip_index=0):
+    def set_per_diem_data(self, begin_date, end_date, travel_per_diem, daily_per_diem):
         """
         Function sets per diem data for a trip
         Input
@@ -250,16 +214,16 @@ class Accountant:
 
         # step 3: set tdy values by calling Trip object methods
         try:
-            self._tdy[trip_index].set_per_diem(daily_per_diem, travel_per_diem)
-            self._tdy[trip_index].set_dates(begin_date, end_date)
+            self._tdy.set_per_diem(daily_per_diem, travel_per_diem)
+            self._tdy.set_dates(begin_date, end_date)
         except ValueError as e:
             raise e
 
-    def calculate_trip_duration(self, trip_index=0):
+    def calculate_trip_duration(self):
         """
-        Function returns the duration of a TDY
+        Function returns the duration of the TDY
         Input
-            trip_index=0 - Trip object to calculate duration for
+            none
         Returns
             duration of trip
         Throws
@@ -268,14 +232,14 @@ class Accountant:
 
         # step 1: call upon Trip object to calculate own duration
         try:
-            result = self._tdy[trip_index].calculate_duration()
+            result = self._tdy.calculate_duration()
         except InvalidOperationError as e:  # this exception occurs if Trip object's dates have not been set
             raise e
 
         # step 2: return the result
         return result
 
-    def calculate_per_diem_total(self, trip_index=0):
+    def calculate_per_diem_total(self):
         """
         Method returns the amount of per diem for the a Trip object
         Input
@@ -288,7 +252,7 @@ class Accountant:
 
         # step 1: call on Trip object to calculate its own per diem total
         try:
-            total = self._tdy[trip_index].calculate_per_diem_total()
+            total = self._tdy.calculate_per_diem_total()
         except InvalidOperationError as e: # exception occurs if Trip object date or per diem values not set
             raise e
 
@@ -315,7 +279,7 @@ class Accountant:
 
             # detect duplicate transaction
             self.find_transaction(new_transaction)
-            
+
             self._transactions.append(new_transaction)
 
             # step 2: sort the transactions by date
@@ -335,11 +299,11 @@ class Accountant:
         Throws: ValueError if param is not transaction or transaction is not found
         """
         # todo: test function
-        
+
         # initialize variables
         result = -1 # stores find_me's location
         temp_index = 0 # keeps track of our location in the list
-        
+
         # check find_me is valid Transaction object
         if not isinstance(find_me, Transaction):
             raise ValueError("Accountant.find_transaction param not valid")
@@ -350,7 +314,7 @@ class Accountant:
             date_same = transaction.transaction_date == find_me.transaction_date
             amount_same = transaction.amount == find_me.amount
             remarks_same = transaction.remarks == find_me.remarks
-            
+
             if amount_same and date_same and amount_same and remarks_same:
                 if not result == -1: # case that we have already found the transaction
                     raise InvalidOperationError("Error: duplicate transactions exist")
@@ -383,11 +347,11 @@ class Accountant:
         # step 3: return the total
         return total
 
-    def calculate_earned_per_diem(self, trip_index=0):
+    def calculate_earned_per_diem(self):
         """
         Calls Trip object to calculate how much per diem we have earned based on the current date
         Input
-            trip_index = 0 - which trip we are talking about
+            none
         Returns
             earned per diem
         Throws
@@ -395,7 +359,7 @@ class Accountant:
         """
 
         # step 1: have the Trip object do the calculation and return the result
-        return self._tdy[trip_index].calculate_earned_per_diem()
+        return self._tdy.calculate_earned_per_diem()
 
     def modify_transaction(self, transaction_number, name, transaction_date, amount, remarks):
         """
@@ -424,27 +388,14 @@ class Accountant:
             self.find_transaction(new_transaction)
         except InvalidOperationError as e:
             raise e
-        
+
         # Step 2: remove transaction with indicated transaction number
         self._transactions.remove(self._transactions[transaction_number - 1])
 
         # Step 3: use bank's own add_transaction function to add transaction
-        self.add_transaction(new_transaction.name, new_transaction.transaction_date, new_transaction.amount, new_transaction.remarks) 
+        self.add_transaction(new_transaction.name, new_transaction.transaction_date, new_transaction.amount, new_transaction.remarks)
 
         return self.find_transaction(new_transaction)
-
-    def trip_parameters_set(self, trip_index=0):
-        """
-        Determines whether a trip's date and per diem values have been set
-        Input
-            trip_index=0 - the trip we are examining
-        Returns
-            answer - boolean value indicating whether the trip has values
-        Throws
-            none
-        """
-        # step 1: combine dates_set() and per_diem_values_set() with "and".  If either is false, method returns false.
-        return self._tdy[trip_index].dates_set() and self._tdy[trip_index].per_diem_values_set()
 
     def num_transactions(self):
         return len(self._transactions)
@@ -457,7 +408,7 @@ class Transaction:
         # todo: change InvalidOperationErrors to ValueErrors
         if isinstance(amount, int): # if amount is an integer, we can convert it to a Decimal
             amount = Decimal(amount)
-        
+
         if not isinstance(name, str) or not isinstance(amount, Decimal) or not isinstance(remarks, str):
             raise InvalidOperationError("Debug error: Transaction init; values passed not valid type")
         if not isinstance(transaction_date, date):
@@ -473,6 +424,8 @@ class Transaction:
         self.remarks = remarks
 
 
+'''
+Removing trip class to be added back at a later date
 class Trip:
     # Class contains beginning and end dates of a trip, per diem info and calculations
     def __init__(self, begin_date = date(1950, 1, 1) , end_date = date(1950, 1, 1), daily_per_diem = -1, travel_per_diem = -1):
@@ -491,7 +444,7 @@ class Trip:
         # step 1: make sure per diem values provided to function are integers or Decimal
         if (not isinstance(daily_per_diem, Decimal) and not isinstance(daily_per_diem, int)) or (not isinstance(travel_per_diem, Decimal) and not isinstance(travel_per_diem, int)):
             raise ValueError('Debug error: Trip.set_per_diem; values passed not Decimal or int')
-             
+
         # step 2: make sure the per diem values provided to function make sense (not negative)
         if daily_per_diem < 0 or travel_per_diem < 0:
             raise ValueError('Debug error: Trip.set_per_diem; values passed must be positive')
@@ -521,7 +474,7 @@ class Trip:
 
     def retrieve_values(self):
         """
-        Function used to retrieve all values from the Trip. 
+        Function used to retrieve all values from the Trip.
         parameters: none
         returns: dictionary containing all values.  These values can be modified without affecting object's variables
         throws: none
@@ -538,7 +491,7 @@ class Trip:
         if self.per_diem_values_set():
              values['daily_per_diem'] = self._daily_per_diem
              values['travel_per_diem'] = self._travel_per_diem
-             
+
         # step 3: return dictionary to caller
         return values
 
@@ -556,7 +509,7 @@ class Trip:
 
         # step 2: if you make it past that first check, values are good and we return true
         return True
-        
+
     def per_diem_values_set(self):
         """
         Function check per diem values to see if they have been set
@@ -660,3 +613,4 @@ class Trip:
         total += (self._daily_per_diem * days)
 
         return total
+'''
